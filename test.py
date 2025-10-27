57fd374a6e9944c8fd1eaf00fa300c8482304516
Taoranzhang1 <672572623@qq.com>
2025-10-27 09:56:45 +0800

    .

---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+
+
+def test_environment_variables():
+    os.environ["APP_NAME"] = "Python程序"
+    os.environ["GREETING"] = "欢迎"
+
+    app_name = os.getenv("APP_NAME")
+    greeting = os.getenv("GREETING")
+
+    assert app_name == "Python程序"
+    assert greeting == "欢迎"
+
+
+def test_default_values():
+    default_app_name = os.getenv("NON_EXISTENT_VAR", "默认应用")
+    assert default_app_name == "默认应用"
+
+
+def test_string_operations():
+    name = "测试用户"
+    assert len(name) == 4
+    assert name + "!" == "测试用户!"
